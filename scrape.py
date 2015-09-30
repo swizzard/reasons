15233f2a494935017b5e700db6ff40af6653070d
Sam Raker <sraker@elitedaily.com>
2015-09-30 08:59:23 -0400

    iterate better (thanks mark), debug

--- scrape.py
+++ scrape.py
@@ -4,24 +4,25 @@ import requests
 
 
 def get_reasons(req):
-    params = {'search': 'reasons',
-            'page': idx}
-
-    posts = requests.get(url, params=params).json()['posts']
+    posts = req.json()['posts']
 
     bodies = [post['post_content'] for post in posts]
 
     p = re.compile(r'^(?:<h2>)?\d+\.(?P<reason>.+?)(?:(\<\/h2\>)?\r)?$', re.M)
     for body in bodies:
         for m in re.finditer(p, body):
-            yield m.group('reason')
+            yield m.group('reason').strip()
 
 
- def get_all_reasons(): 
-    idx = 0
-    saw_stopiteration = False
+def get_all_reasons(): 
+    idx = 1
     url = 'http://elitedaily.com/api/get_search_results'
-    while True:
+    req = requests.get(url, params={'search': 'reasons', 'page': 1})
+    page_count = req.json()['pages']
+    reasons = get_reasons(req)
+    for reason in reasons:
+        yield reason
+    while idx < pages:
         req = requests.get(url, params={'search': 'reasons', 'page': idx})
         reasons = get_reasons(req)
         while True:
